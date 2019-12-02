e0b6ad5ab7f3c70a84049bd322b125fc7d8c5ab3
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2019-12-02 18:20:31 +0100

    added TryWithResources class

---
+++ src/main/java/exceptions/tryWithResources/TryWithResourcesExample.java
@@ -0,0 +1,23 @@
+package exceptions.tryWithResources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    // aby użyć instrukcji TWR muszę mieć klasę która
+    // implementuje interfejs AutoCloseable
+    // przykładem takiej klasy są BufferReader/Writer
+    
+    public void printLinesOfFile(String name){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(name))){
+            String currentLine = bufferedReader.readLine();
+            while(currentLine != null){
+                System.out.println(currentLine);
+                currentLine = bufferedReader.readLine();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
