b02a7626a4e269498364fa8c2225ec1e1b67eb9e
Mateusz Grodzki <mateusz.maria.grodzki@gmail.com>
2020-02-11 12:38:10 +0100

    added HiddenClass to relfection package

---
+++ src/main/java/reflection/HiddenClass.java
@@ -0,0 +1,11 @@
+package reflection;
+
+public class HiddenClass {
+
+    private String text = "Prywatne pole";
+
+    private HiddenClass(){
+        System.out.println("Prywatny kosntruktor");
+        text = "Zmiana prywatnego pola przez konstruktor";
+    }
+}
